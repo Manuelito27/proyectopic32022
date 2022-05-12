3558cfc5cb2b30afe5e819b64a84478a4ee9c76c
alfonsoHR98 <alfonsohr_98@outlook.com>
2022-05-12 18:30:55 -0500

    Insercion de pagina de inicio de la plataforma

---
+++ Ingles Kids 2/inicio.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8" />
+        <title>Ingles Kids</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" type="text/css" href="estilos/inicio.css" />
+    </head>
+    <body>
+        
+    </body>
+</html>
