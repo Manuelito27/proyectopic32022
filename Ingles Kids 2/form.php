f1a47735fd52e82bda0ec2b6ad7eb3ad625e8f0c
= <=>
2022-05-12 11:02:24 -0500

    x

--- Ingles Kids 2/form.php
+++ Ingles Kids 2/form.php
@@ -1,7 +1,8 @@
 <?php
     //Conexion con BD
     require 'conexion.php';
-        session_start();
+    session_start();
+    
     $apellidoP = $_POST['ApellidoP'];
     $apellidoM = $_POST['ApellidoM'];
     $nombre = $_POST['Nombre'];
@@ -10,8 +11,14 @@
     $rol = $_POST['Rol'];
     $genero = $_POST['Genero'];
 
-    if($passc!=$pass){
-        alert("MENSAJE");
+    if($apellidoP==""){
+        echo 'No se escribio Apellido Paterno';
+    }else if($nombre==""){
+        echo 'No se escribio un nombre';
+    }else if($pass==""){
+        echo 'No se escribio ninguna contraseña';
+    }else if($passc!=$pass){
+        echo 'Las contraseñas no son iguales';
     }else{
         if($rol=="Docente"){
             $sql = "INSERT INTO docente (Nombre,ApellidoP,ApellidoM,Pass)
@@ -21,6 +28,7 @@
             VALUES ('$nombre','$apellidoP','$apellidoM','$pass')";
         }
         $resultados = mysqli_query($con,$sql) or die('Error en la database');
+        header("location: index.php");
         mysqli_close($con);
     }
 ?>
